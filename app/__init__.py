fdd67e3c6bb503fd394ea84a2cae3758afc6f137
Talich12 <denis.tal@mail.ru>
2023-05-04 19:48:12 +1000

    edit_config_and_import_flask-marshmallow

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db,render_as_batch=True)
 cors = CORS(app)
+ma = Marshmallow(app)
 
 
 @app.after_request
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 4
     JWT_SECRET_KEY = 'jwt-secret-string'
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
