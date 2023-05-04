3c1d632949f2683185823caa0a0be3f6f3193bb5
Talich12 <denis.tal@mail.ru>
2023-05-04 19:44:52 +1000

    edit_relationships

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
-from app import db
+from app import db, ma
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from marshmallow_sqlalchemy import fields, auto_field
 from hashlib import md5
 import base64
 from datetime import datetime, timedelta
@@ -11,6 +12,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     rating = db.Column(db.Integer, default=0)
+    
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -26,7 +28,8 @@ class Post(db.Model):
     title = db.Column(db.String(140))
     body = db.Column(db.String(3000))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author = db.relationship("User", backref="books")
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
@@ -47,3 +50,25 @@ class RevokedTokenModel(db.Model):
     
     def __repr__(self):
         return '<Token {}>'.format(self.body)
+
+
+class UserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+        load_instance = True
+
+    id = ma.auto_field()
+    username = ma.auto_field()
+
+
+class PostSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Post
+        load_instance = True
+
+    id = auto_field()
+    title = auto_field()
+    body = auto_field()   
+
+    author = fields.Nested(UserSchema) 
+    
