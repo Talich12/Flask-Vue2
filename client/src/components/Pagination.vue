3b65602fc229d8a335b24d853c22afdc2e77f3c2
talich12 <denis.tal@mail.ru>
2023-05-23 20:53:38 +1000

    add_work_pagination

--- client/src/components/Pagination.vue
+++ client/src/components/Pagination.vue
@@ -1,15 +1,22 @@
 <template> 
     <div class="center con-pagination" style="margin-top: 7vh;">
-        <vs-pagination not-margin color="danger" progress v-model="page" :length="20" />
+        <vs-pagination not-margin color="danger" progress v-model="page" :length="len" />
     </div>
 </template>
 
 <script>
 export default {
+    props: ['len'],
     data:() => ({
         page: 1,
     }),
-}
+    watch: {
+        page: function(){
+            this.$emit('page', {page: this.page})
+        }
+    }
+}   
+
 </script>
 
 <style>
